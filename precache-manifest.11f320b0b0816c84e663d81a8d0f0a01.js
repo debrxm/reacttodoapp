a1adaa1a986f4e236fb70ed6258b061e67418a4f
0zzycodes <horlalekan123@gmail.com>
2019-05-24 16:27:18 +0800

    Updates

---
+++ precache-manifest.11f320b0b0816c84e663d81a8d0f0a01.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f98154755bfa589d5571e17599fe4b",
+    "url": "/reacttodoapp/index.html"
+  },
+  {
+    "revision": "c62f0163febdd3a96069",
+    "url": "/reacttodoapp/static/css/main.aac04dd2.chunk.css"
+  },
+  {
+    "revision": "4fa652ad62e428bdd49f",
+    "url": "/reacttodoapp/static/js/2.e9b4797e.chunk.js"
+  },
+  {
+    "revision": "c62f0163febdd3a96069",
+    "url": "/reacttodoapp/static/js/main.617ac7fc.chunk.js"
+  },
+  {
+    "revision": "7247e3d1572c33ad1d83",
+    "url": "/reacttodoapp/static/js/runtime~main.a7ad2f43.js"
+  },
+  {
+    "revision": "4d535f540e6f1a647b9069b792ca0b0d",
+    "url": "/reacttodoapp/static/media/add.4d535f54.svg"
+  },
+  {
+    "revision": "e63f9cd74f3445f9aa82e7cf090ea4e2",
+    "url": "/reacttodoapp/static/media/bin.e63f9cd7.svg"
+  },
+  {
+    "revision": "b31a223327c88c84c086e5a272b98797",
+    "url": "/reacttodoapp/static/media/checked.b31a2233.svg"
+  },
+  {
+    "revision": "d17ac4279e08d1efb6897dbc4a6526c5",
+    "url": "/reacttodoapp/static/media/menu.d17ac427.svg"
+  },
+  {
+    "revision": "ac7e2298b9e528e81fa16300e8897831",
+    "url": "/reacttodoapp/static/media/outline.ac7e2298.svg"
+  },
+  {
+    "revision": "389454698fb2a44c11e6c3fa64487074",
+    "url": "/reacttodoapp/static/media/settings.38945469.svg"
+  },
+  {
+    "revision": "334d5b0a62f591e1b8fe03eed1c2b717",
+    "url": "/reacttodoapp/static/media/shop.334d5b0a.svg"
+  }
+]);
