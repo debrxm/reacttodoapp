45f054b97110449230df0a1853f2b52da6606a9b
0zzycodes <horlalekan123@gmail.com>
2019-05-25 03:24:19 +0800

    adjust settings

--- src/components/Setting/Setting.js
+++ src/components/Setting/Setting.js
@@ -1,32 +1,11 @@
 import React, { Component } from 'react';
 import './Setting.css';
 class Setting extends Component {
-  defaultTheme() {
-    console.log('Default');
-  }
-  darkTheme() {
-    console.log('Dark');
-  }
-  mixTheme() {
-    console.log('Mix');
-  }
   render() {
     return (
       <div className="Setting">
-        <div className="Theme">
-          <h2>
-            Themes:
-            <span className="Default" onClick={this.defaultTheme}>
-              Default
-            </span>
-            <span className="Dark" onClick={this.darkTheme}>
-              Dark
-            </span>
-            <span className="Mix" onClick={this.mixTheme}>
-              Mix
-            </span>{' '}
-          </h2>
-        </div>
+        <h3>Settings</h3>
+        <h2>Not Available</h2>
       </div>
     );
   }
